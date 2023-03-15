611faeca9e5e2c7ba29d91cf0c49fc3f5f9071b3
zali1205 <zali1205@gmail.com>
2023-03-14 23:38:32 -0400

    complete functionality for decode and encode and properly handles file input and output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from morse import morse_dict
 
 def find_file():
-    print("Please place your text file to be encoded into the 'input' folder.")
+    print("Please place your text file to be encoded or decoded into the 'input' folder.")
     while True:
         user_input = input("Please enter the full name of the file to be encoded.\n")
         try:
@@ -11,23 +11,27 @@ def find_file():
         except FileNotFoundError as e:
             print(f"{e}")
             print("File does not exist! Please try again!")
+        except IOError as e:
+            print(f"{e}")
+            print("Could not read the file! Please make sure the file to be read has a .txt extension!")
     return data_lines
 
-def ouput_to_file(encoded_text: str):
+def output_to_file(encoded_lines: list):
     print("The output file will be placed in the 'output' folder.")
     while True:
         user_input = input("Please enter the name for your text file.\n")
         try:
-            with open(user_input, 'w') as file:
-                file.write(encoded_text)
+            with open(f"output/{user_input}.txt", 'w') as file:
+                for line in encoded_lines: 
+                    file.write(line.upper() + "\n")
                 break
-        except FileNotFoundError as e:
+        except FileExistsError as e:
             print(f"{e}")
-            print("File does not exist! Please try again!")
+            print("File already exists! Please try again!")
     print("Successfully created the output file!")
 
 def encode():
-    user_input = input("Would you like to encode a text file or enter text manually? F - File | Enter - Text/Continue \n").lower()
+    user_input = input("Would you like to encode a text file or enter the text manually? F - File | Enter - Manual Input \n").lower()
     data_lines = []
     if user_input == "f":
         data_lines = find_file()
@@ -48,7 +52,7 @@ def encode():
         encoded_lines.append(encoded_text)
         encoded_text = ""
 
-    user_input = ("Encoding complete! Would you like to print to console or to an output file? F - File | Enter - Console").lower()
+    user_input = input("Encoding complete! Would you like to print to console or to an output file? F - File | Enter - Console\n").lower()
     if user_input == "f":
         output_to_file(encoded_lines)
     else:
@@ -56,33 +60,51 @@ def encode():
             print(line)
 
 def decode():
-    user_input = input("Please enter the Morse code you would like to decode.\n").lower()
+    user_input = input("Would you like to decode a text file or enter the text manually? F - File | Enter - Manual Input \n").lower()
+    data_lines = []
+    if user_input == "f":
+        data_lines = find_file()
+    else:
+        data = input("Please enter the text you would like to encode into Morse code.\n").lower() 
+        data_lines.append(data)
     
-    morse_words = []
-    morse_letter_to_add = ""
-    for count, char in enumerate(user_input):
-        if char == " ":
+    morse_lines = []
+
+    for line in data_lines:
+        morse_words = []
+        morse_letter_to_add = ""
+        for char in line.strip():
+            if char == " ":
+                morse_words.append(morse_letter_to_add)
+                morse_letter_to_add = ""
+            elif char == "/":
+                morse_words.append(morse_letter_to_add)
+                morse_words.append(" ")
+                morse_letter_to_add = ""
+            else:
+                morse_letter_to_add += char
+        if morse_letter_to_add != "":
             morse_words.append(morse_letter_to_add)
-            morse_letter_to_add = ""
-        elif char == "/":
-            morse_words.append(morse_letter_to_add)
-            morse_words.append(" ")
-            morse_letter_to_add = ""
-        else:
-            morse_letter_to_add += char
-    if morse_letter_to_add != "":
-        morse_words.append(morse_letter_to_add)
+        morse_lines.append(morse_words)
 
     morse_to_text_dict = {v: k for k, v in morse_dict.items()}
-    decoded_text = ""
+    decoded_lines = []
 
-    for word in morse_words:
-        if word == " ":
-            decoded_text += " "
-        else:
-            decoded_text += morse_to_text_dict.get(word, word)
+    for line in morse_lines:
+        decoded_text = ""
+        for word in line:
+            if word == " ":
+                decoded_text += " "
+            else:
+                decoded_text += morse_to_text_dict.get(word, word)
+        decoded_lines.append(decoded_text)
         
-    print(decoded_text)
+    user_input = input("Decoding complete! Would you like to print to console or to an output file? F - File | Enter - Console\n").lower()
+    if user_input == "f":
+        output_to_file(decoded_lines)
+    else:
+        for line in decoded_lines:
+            print(line.upper())
 
 continueProgram = True
 
